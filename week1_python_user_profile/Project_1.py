1dffc59dc4055d9fc0ec086a0a8d310ef9b5511e
Rudranshu-Raj <rajrudranshu10.02@gmail.com>
2026-08-12 07:32:12 +0530

    Delete week1_python_user_profile directory

--- week1_python_user_profile/Project_1.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name?")
-age = input("How old are you?")
-hobby = input("What is your favorite hobby?")
-
-print(f"Welcome {name}!")
-print(f"You are {age} years old and love {hobby}.")
